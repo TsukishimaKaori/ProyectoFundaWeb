c0eae0fc984511cf88116c35cebfd9ab45a7f288
HP_810G2 <HP_810G2@DESKTOP-HFVR94I>
2018-04-16 10:01:52 -0600

    Funda web Fran

--- Posiciones.php
+++ Posiciones.php
@@ -1,4 +1,51 @@
-<?php
-
+<html>
+    <head>        
+        <title>Alemania </title>
+         <link href="../recursos/css/posiciones.css" rel="stylesheet"/>
+    </head>
+    <body>
+        
+    <?php
+    crearTabla();
+    function crearTabla(){
+        echo '<table border = 2 id = "tabla-principal">
+                <thead>
+                <tr>
+                    <th>Equipo</th>
+                    <th>PJ</th>
+                    <th>PG</th>
+                    <th>PE</th>
+                    <th>PP</th>
+                    <th>GF</th>
+                    <th>GC</th>
+                    <th>Dif</th>
+                    <th>Puntos</th>
+                    <th>Ujuegos</th>
+                 </tr>
+                </thead>
+                <tbody>
+                
+                 <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                 </tr>
+                 
+                </tbody>
+            </table>';
+    }
+      ?>  
+        
+    </body>
+</html>
+        
+        
 
 
