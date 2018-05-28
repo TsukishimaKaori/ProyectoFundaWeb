b1166d756703c7e0f8c8d3308eeb2ea514318ce3
HP_810G2 <HP_810G2@DESKTOP-HFVR94I>
2018-05-28 10:47:04 -0600

    Fran

--- Conexion.php
+++ Conexion.php
@@ -1,7 +1,12 @@
 <?php
 
 function establecerConexion() {
-    $con = mysqli_connect('localhost', 'root', 'root', 'futbol');
+
+    $con = mysqli_connect('localhost', 'root', '', 'futbol');
+       if (!$con->set_charset("utf8")) {
+        printf("Error cargando el conjunto de caracteres utf8: %s\n", $c->error);
+        exit();
+    }
     if (mysqli_connect_errno()) {
         echo "Falló la conexión: " . mysqli_connect_errno();
         exit();
